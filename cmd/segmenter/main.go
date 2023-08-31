4ecfa7e0b33df48bd312963b869ece033edd8607
elhem <yandra_5@bk.ru>
2023-08-31 14:36:08 +0400

    Add server struct Add Config init

--- cmd/segmenter/main.go
+++ cmd/segmenter/main.go
@@ -1,7 +1,23 @@
 package main
 
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
 func main() {
-	//TODO: Init config
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+
+	if err := InitConfig(); err != nil {
+		logrus.Fatalf("Config init error: %s", err.Error())
+	}
+
 	//TODO: Init db
 	//TODO: Init server
+
+}
+func InitConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
